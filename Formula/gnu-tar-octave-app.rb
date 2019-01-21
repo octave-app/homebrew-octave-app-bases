dc11e8e4d5fcfaf2c146015923da82212e434300
Andrew Janke <floss@apjanke.net>
2019-01-20 22:38:41 -0500

    gnu-tar: bump to 1.31

--- Formula/gnu-tar-octave-app.rb
+++ Formula/gnu-tar-octave-app.rb
@@ -1,11 +1,20 @@
+# Customized to install without "g" prefixes by default, so we can
+# get "tar" as a GNU tar on the path by default; Octave isn't compatible
+# with BSD tar (yet; see https://savannah.gnu.org/bugs/index.php?53695).
 class GnuTarOctaveApp < Formula
   desc "GNU version of the tar archiving utility"
   homepage "https://www.gnu.org/software/tar/"
-  url "https://ftp.gnu.org/gnu/tar/tar-1.30.tar.gz"
-  mirror "https://ftpmirror.gnu.org/tar/tar-1.30.tar.gz"
-  sha256 "4725cc2c2f5a274b12b39d1f78b3545ec9ebb06a6e48e8845e1995ac8513b088"
+  url "https://ftp.gnu.org/gnu/tar/tar-1.31.tar.gz"
+  mirror "https://ftpmirror.gnu.org/tar/tar-1.31.tar.gz"
+  sha256 "b471be6cb68fd13c4878297d856aebd50551646f4e3074906b1a74549c40d5a2"
 
-  option "without-default-names", "Prepend 'g' to the binary"
+  head do
+    url "https://git.savannah.gnu.org/git/tar.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+  end
 
   def install
     # Work around unremovable, nested dirs bug that affects lots of
@@ -16,41 +25,19 @@ class GnuTarOctaveApp < Formula
     # https://lists.gnu.org/archive/html/bug-tar/2015-10/msg00017.html
     ENV["gl_cv_func_getcwd_abort_bug"] = "no" if MacOS.version == :el_capitan
 
-    args = ["--prefix=#{prefix}", "--mandir=#{man}"]
-    args << "--program-prefix=g" if build.without? "default-names"
+    args = %W[
+      --prefix=#{prefix}
+      --mandir=#{man}
+    ]
 
+    system "./bootstrap" if build.head?
     system "./configure", *args
     system "make", "install"
-
-    # Symlink the executable into libexec/gnubin as "tar"
-    if build.without? "default-names"
-      (libexec/"gnubin").install_symlink bin/"gtar" =>"tar"
-      (libexec/"gnuman/man1").install_symlink man1/"gtar.1" => "tar.1"
-    end
-  end
-
-  def caveats
-    if build.without? "default-names" then <<~EOS
-      gnu-tar has been installed as "gtar".
-
-      If you really need to use it as "tar", you can add a "gnubin" directory
-      to your PATH from your bashrc like:
-
-          PATH="#{opt_libexec}/gnubin:$PATH"
-
-      Additionally, you can access their man pages with normal names if you add
-      the "gnuman" directory to your MANPATH from your bashrc as well:
-
-          MANPATH="#{opt_libexec}/gnuman:$MANPATH"
-
-    EOS
-    end
   end
 
   test do
-    tar = build.with?("default-names") ? bin/"tar" : bin/"gtar"
     (testpath/"test").write("test")
-    system tar, "-czvf", "test.tar.gz", "test"
-    assert_match /test/, shell_output("#{tar} -xOzf test.tar.gz")
+    system bin/"tar", "-czvf", "test.tar.gz", "test"
+    assert_match /test/, shell_output("#{bin}/tar -xOzf test.tar.gz")
   end
 end
