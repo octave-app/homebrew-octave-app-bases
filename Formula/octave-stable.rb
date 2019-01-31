87e8c1e36a495acc4d5e1280da76c467d3cd41b9
Andrew Janke <floss@apjanke.net>
2019-01-31 02:57:52 -0500

    Downgrade to bison 3.2 because bison 3.3 breaks the build

--- Formula/octave-stable.rb
+++ Formula/octave-stable.rb
@@ -39,7 +39,7 @@ class OctaveStable < Formula
   depends_on "pkg-config" => :build
   # Head-specific build dependencies
   depends_on "mercurial" => :build
-  depends_on "bison" => :build
+  depends_on "bison@3.2" => :build
   depends_on "doxygen" => :build
   depends_on "icoutils" => :build
   depends_on "librsvg" => :build
