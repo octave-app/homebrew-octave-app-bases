b32ed223527e2d41b4f8d236e0c01848c2c1459c
Andrew Janke <floss@apjanke.net>
2019-03-05 03:36:00 -0500

    qterminal: add qtermwidget dependency

--- Formula/qterminal.rb
+++ Formula/qterminal.rb
@@ -8,24 +8,12 @@ class Qterminal < Formula
   depends_on "cmake" => :build
   depends_on "lxqt-build-tools" => :build
   depends_on "qt"
+  depends_on "qtermwidget"
 
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test qterminal`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
